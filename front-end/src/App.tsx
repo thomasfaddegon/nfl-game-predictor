31249c41ff3dbd1a1519229800e931a20ee34a4f
Thomas Faddegon <tomfaddegon@gmail.com>
2024-03-13 11:17:56 -0400

    added front end form and error handling

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,6 +4,8 @@ import { nflTeams } from "../utils.ts";
 
 function App() {
   const [teams, setTeams] = useState({ awayTeam: "", homeTeam: "" });
+  const [result, setResult] = useState("");
+  const [error, setError] = useState("");
 
   const handleTeamChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = event.target;
@@ -13,14 +15,50 @@ function App() {
     }));
   };
 
+  const errorCheck = () => {
+    if (
+      !teams.awayTeam ||
+      !teams.homeTeam ||
+      teams.awayTeam === "Select a Team" ||
+      teams.homeTeam === "Select a Team"
+    ) {
+      setError("Please select two teams");
+      return true;
+    }
+    if (teams.awayTeam === teams.homeTeam) {
+      setError("Please select different teams");
+      return true;
+    }
+    setError("");
+    return false;
+  };
+
+  const handleSubmit = () => {
+    if (errorCheck()) return;
+    fetch("/api/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(teams),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
-    <>
-      <h1 className="mb-24">NFL Game Predictor</h1>
-      <div className="flex flex-row">
+    <div className="flex flex-col items-center">
+      <h1 className="mb-8">NFL Game Predictor</h1>
+      <div className="flex max-w-sm gap-8 justify-between flex-row">
         <div>
           <select
             id="homeTeamsDropdown"
-            name="home"
+            name="homeTeam"
             onChange={handleTeamChange}
           >
             <option>Select a Team</option>
@@ -35,7 +73,7 @@ function App() {
         <div>
           <select
             id="homeTeamsDropdown"
-            name="home"
+            name="awayTeam"
             onChange={handleTeamChange}
           >
             <option>Select a Team</option>
@@ -47,7 +85,16 @@ function App() {
           </select>
         </div>
       </div>
-    </>
+      {error && <p className="text-red-500">{error}</p>}
+      <div>
+        <button
+          onClick={handleSubmit}
+          className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Predict
+        </button>
+      </div>
+    </div>
   );
 }
 
